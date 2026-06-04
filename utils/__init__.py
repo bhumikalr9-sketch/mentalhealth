08c21424f036d179b83f4f5d3db13b1dcd8e9365
bhumikalr9-sketch <bhumikalr9@gmail.com>
2026-06-04 16:04:15 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,17 +1,1 @@
-# utils/__init__.py
 
-from .data_loader import load_data
-from .ml_model import (
-    train_depression_model,
-    predict_depression,
-    train_risk_model,
-    predict_risk_score
-)
-
-__all__ = [
-    "load_data",
-    "train_depression_model",
-    "predict_depression",
-    "train_risk_model",
-    "predict_risk_score"
-]
